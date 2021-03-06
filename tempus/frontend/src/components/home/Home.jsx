de62b8de0aca6cf3ec34980c4e42e1bacc3dd569
Greynowver <andy_carvalho18@hotmail.com>
2021-03-06 18:50:17 +0000

    Home & Main

---
+++ tempus/frontend/src/components/home/Home.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import Main from '../template/Main'
+
+export default props =>
+    <Main icon="home" title="Início"
+        subtitle="Sistema de Administração de Clientes">
+        <div className='display-4'>Bem Vindo!</div>
+        <hr />
+        <p className="mb-0">Sistema de Cadastro desenvolvido em React</p>
+    </Main>
